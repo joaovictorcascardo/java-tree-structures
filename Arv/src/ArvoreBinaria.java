398e105fd450571ee970877849d6f3298edce8a8
João Victor Pacheco <joaovictorpachecoleite@gmail.com>
2025-10-28 02:04:00 -0300

    Cria classe ArvoreBinaria e implementa método inserir

--- Arv/src/ArvoreBinaria.java
+++ Arv/src/ArvoreBinaria.java
@@ -1,8 +1,45 @@
-public class ArvoreBinaria{
+public class ArvoreBinaria {
+
     No raiz;
 
-    public ArvoreBinaria(){
+    public ArvoreBinaria() {
         this.raiz = null;
     }
 
+    public void inserir(int dado) {
+        
+        No novoNo = new No(dado);
+
+        if (this.raiz == null) {
+            this.raiz = novoNo;
+            return;
+        }
+
+        No noAtual = this.raiz;
+
+        while (true) {
+
+            if (dado < noAtual.dado) {
+                
+                if (noAtual.esquerda == null) {
+                    noAtual.esquerda = novoNo;
+                    return;
+                } else {
+                    noAtual = noAtual.esquerda;
+                }
+
+            } else if (dado > noAtual.dado) {
+
+                if (noAtual.direita == null) {
+                    noAtual.direita = novoNo;
+                    return;
+                } else {
+                    noAtual = noAtual.direita;
+                }
+            } else {
+                System.out.println("Valor " + dado + " já existe.");
+                return;
+            }
+        }
+    }
 }
